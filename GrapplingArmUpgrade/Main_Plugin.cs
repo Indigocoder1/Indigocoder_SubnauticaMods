8d39d7cb377cbefc61c2ad6b5de8b68cbd8c7c3b
Indigocoder1 <caleb@liston.us>
2023-07-10 23:11:53 -0700

    Added link to inspiration
    Added the link to the mod that inspired this port.
    
    Also found here: https://www.nexusmods.com/subnautica/mods/368

--- GrapplingArmUpgrade/Main_Plugin.cs
+++ GrapplingArmUpgrade/Main_Plugin.cs
@@ -4,6 +4,10 @@ using BepInEx.Logging;
 
 namespace GrapplingArmUpgrade_BepInEx
 {
+    //Please note that this mod is heavily inspired by a mod made by zorgesho#9929
+    //You can find his mod on NexusMods here: https://www.nexusmods.com/subnautica/mods/368
+    //At time of writing the mod only works for Legacy which is the reason for this port
+
     [BepInPlugin(myGUID, pluginName, versionString)]
     public class Main_Plugin : BaseUnityPlugin
     {
