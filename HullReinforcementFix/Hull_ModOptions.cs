f423c97a4f1074a1223ea55d481f429f71489f2d
Indigocoder1 <caleb@liston.us>
2023-08-24 21:28:59 -0700

    Created hull reinforcement fix

---
+++ HullReinforcementFix/Hull_ModOptions.cs
@@ -0,0 +1,30 @@
+﻿using Nautilus.Handlers;
+using Nautilus.Options;
+
+namespace HullReinforcementFix
+{
+    internal class Hull_ModOptions : ModOptions
+    {
+        public Hull_ModOptions() : base("Hull Reinforcement Fix Options")
+        {
+            var writeLogsOption = Main_Plugin.WriteLogs.ToModToggleOption();
+            writeLogsOption.OnChanged += WriteLogsOption_OnChanged; ;
+            AddItem(writeLogsOption);
+
+            var damageReductionOption = Main_Plugin.DamageReductionMultiplier.ToModSliderOption(step: 0.1f, floatFormat: "{0:F1}");
+            damageReductionOption.OnChanged += DamageReductionOption_OnChanged;
+            AddItem(damageReductionOption);
+
+            OptionsPanelHandler.RegisterModOptions(this);
+        }
+
+        private void WriteLogsOption_OnChanged(object sender, ToggleChangedEventArgs e)
+        {
+            Main_Plugin.WriteLogs.Value = e.Value;
+        }
+        private void DamageReductionOption_OnChanged(object sender, SliderChangedEventArgs e)
+        {
+            Main_Plugin.DamageReductionMultiplier.Value = e.Value;
+        }
+    }
+}
---
+++ HullReinforcementFix/Main_Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace HullReinforcementFix
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class Main_Plugin : BaseUnityPlugin
+    {
+        private const string myGUID = "Indigocoder.HullReinforcementFix";
+        private const string pluginName = "Hull Reinforcement Fix";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ConfigEntry<bool> WriteLogs;
+        public static ConfigEntry<float> DamageReductionMultiplier;
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            logger = Logger;
+
+            harmony.PatchAll();
+
+            SetupConfigs();
+            new Hull_ModOptions();
+
+            Logger.LogInfo($"{pluginName} {versionString} Loaded.");
+        }
+
+        private void SetupConfigs()
+        {
+            WriteLogs = Config.Bind("Hull Reinforcement Fix", "Write Logs", false);
+
+            DamageReductionMultiplier = Config.Bind("Hull Reinforcement Fix", "Damage Reduction Multiplier", 1f,
+                new ConfigDescription("How much damage is reduced per module", acceptableValues: new AcceptableValueRange<float>(0.1f, 10)));
+        }
+    }
+}
