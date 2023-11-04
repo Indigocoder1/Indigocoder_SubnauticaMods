9a0396f0fc24429435b80a605af79a6e8c6f7276
Indigocoder1 <caleb@liston.us>
2023-11-04 14:21:27 -0700

    Updated piracy implementation

--- IndigocoderLib/PiracyDetector.cs
+++ IndigocoderLib/PiracyDetector.cs
@@ -7,20 +7,13 @@ using static CraftData;
 using UnityEngine;
 using UWE;
 using System.IO;
+using System.Reflection;
 
 namespace IndigocoderLib
 {
     public static class PiracyDetector
     {
         public static readonly HashSet<string> PiracyFiles = new HashSet<string> { "steam_api64.cdx", "steam_api64.ini", "steam_emu.ini", "valve.ini", "chuj.cdx", "SteamUserID.cfg", "Achievements.bin", "steam_settings", "user_steam_id.txt", "account_name.txt", "ScreamAPI.dll", "ScreamAPI32.dll", "ScreamAPI64.dll", "SmokeAPI.dll", "SmokeAPI32.dll", "SmokeAPI64.dll", "Free Steam Games Pre-installed for PC.url", "Torrent-Igruha.Org.URL", "oalinst.exe", };
-        public static RecipeData recipe = new RecipeData
-        {
-            craftAmount = 0,
-            Ingredients = new List<Ingredient>
-            {
-                new Ingredient(TechType.None, amount: 0)
-            }
-        };
 
         public static bool TryFindPiracy()
         {
@@ -28,35 +21,15 @@ namespace IndigocoderLib
             {
                 if (File.Exists(Path.Combine(Environment.CurrentDirectory, file)))
                 {
-                    GotEm();
+                    Console.WriteLine($"{Assembly.GetExecutingAssembly().GetName()} does not support piracy!");
+                    ErrorMessage.AddError($"{Assembly.GetExecutingAssembly().GetName()} does not support piracy!");
+                    Console.WriteLine("You can purchase the game at discounted prices here: https://isthereanydeal.com/game/subnautica/info/");
+                    ErrorMessage.AddError("You can purchase the game at discounted prices here https://isthereanydeal.com/game/subnautica/info/");
                     return true;
                 }
             }
 
             return false;
-        }
-
-        public static void GotEm()
-        {
-            CoroutineHost.StartCoroutine(LogError());
-            foreach (TechType techType in Enum.GetValues(typeof(TechType)))
-            {
-                LanguageHandler.SetTechTypeName(techType, " ");
-                LanguageHandler.SetTechTypeTooltip(techType, " ");
-                CraftDataHandler.SetRecipeData(techType, recipe);
-            }
-        }
-
-        public static IEnumerator LogError()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(1);
-                Console.WriteLine("Aaarg matey! No pirates be allowed here!");
-                ErrorMessage.AddError("Aaarg matey! No pirates be allowed here!");
-                Console.WriteLine("Purchase the game at discounted prices here: https://isthereanydeal.com/game/subnautica/info/");
-                ErrorMessage.AddError("Purchase the game at discounted prices here: https://isthereanydeal.com/game/subnautica/info/");
-            }
         }
     }
 }
