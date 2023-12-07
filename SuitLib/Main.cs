97d9e4de154e303272d5d6855c4365401c73a09d
Indigocoder1 <caleb@liston.us>
2023-12-06 18:34:18 -0800

    Removed saving of example data

--- SuitLib/Main.cs
+++ SuitLib/Main.cs
@@ -34,33 +34,9 @@ namespace SuitLib
             yield return stillsuitTask;
             waterPefab = stillsuitTask.result.value.GetComponent<Stillsuit>().waterPrefab;
 
-            SaveExampleData();
             LoadJsons();
 
             Logger.LogInfo($"{pluginName} {versionString} Loaded.");
-        }
-
-        private void SaveExampleData()
-        {
-            if (!File.Exists(Path.Combine(jsonFolder, "exampleSuit.json")))
-            {
-                logger.LogWarning("Saving example data. You may see Nautilus file errors");
-                Dictionary<string, string> suitKVP = new Dictionary<string, string> { { "_MainTex", "MySuitTexture.png" } };
-                Dictionary<string, string> armsKVP = new Dictionary<string, string> { { "_MainTex", "MyArmsTexture.png" } };
-                ModdedSuit exampleSuit = new ModdedSuit(suitKVP, armsKVP, ModdedSuitsManager.VanillaModel.Reinforced,
-                    ModdedSuitsManager.Modifications.Reinforced);
-
-                JsonLoader<ModdedSuit>.SaveToJson(new List<ModdedSuit> { exampleSuit }, Path.Combine(jsonFolder, "exampleSuit.json"));
-            }
-
-            if (!File.Exists(Path.Combine(jsonFolder, "exampleGloves.json")))
-            {
-                logger.LogWarning("Saving example data. You may see Nautilus file errors");
-                Dictionary<string, string> glovesKVP = new Dictionary<string, string> { { "_MainTex", "MyGlovesTexture.png" } };
-                ModdedGloves exampleGloves = new ModdedGloves(glovesKVP, ModdedSuitsManager.VanillaModel.Reinforced, ModdedSuitsManager.Modifications.Reinforced);
-
-                JsonLoader<ModdedGloves>.SaveToJson(new List<ModdedGloves> { exampleGloves }, Path.Combine(jsonFolder, "exampleGloves.json"));
-            }
         }
 
         private void LoadJsons()
