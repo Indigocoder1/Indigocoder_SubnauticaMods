fb93817e960f077fc78e5b5f038515261a345ed3
Indigocoder1 <caleb@liston.us>
2023-12-10 10:03:24 -0800

    Added the ignoring of example data

--- SuitLib/Main.cs
+++ SuitLib/Main.cs
@@ -17,7 +17,7 @@ namespace SuitLib
     {
         private const string myGUID = "Indigocoder.SuitLib";
         private const string pluginName = "SuitLib";
-        private const string versionString = "1.0.8";
+        private const string versionString = "1.0.9";
 
         public static ManualLogSource logger;
         private static readonly Harmony harmony = new Harmony(myGUID);
@@ -42,22 +42,38 @@ namespace SuitLib
 
         private void LoadJsons()
         {
+            AddModdedSuits();
+            logger.LogInfo("Finished loading suit Jsons");
+
+            AddModdedGloves();
+            logger.LogInfo("Finished loading gloves Jsons");
+        }
+
+        private void AddModdedSuits()
+        {
             List<JsonSuit> suits = JsonLoader<JsonSuit>.LoadJsons(jsonFolder);
 
             foreach (JsonSuit suit in suits)
             {
-                if(suit == null) continue;
+                if (suit == null) continue;
 
                 Dictionary<string, Texture2D> suitKVPs = new Dictionary<string, Texture2D>();
                 Dictionary<string, Texture2D> armKVPs = new Dictionary<string, Texture2D>();
+                bool example = false;
 
-                if(suit.suitFileNamePairs == null || suit.armFileNamePairs == null)
+                if (suit.suitFileNamePairs == null || suit.armFileNamePairs == null)
                 {
                     continue;
                 }
 
                 foreach (var suitKey in suit.suitFileNamePairs.Keys)
                 {
+                    if (suit.suitFileNamePairs[suitKey].ToLower().Contains("example"))
+                    {
+                        example = true;
+                        break;
+                    }
+
                     string path = Path.Combine(jsonTexturesFolder, suit.suitFileNamePairs[suitKey]);
                     suitKVPs.Add(suitKey, ImageUtils.LoadTextureFromFile(path));
                 }
@@ -67,13 +83,20 @@ namespace SuitLib
                     armKVPs.Add(armsKey, ImageUtils.LoadTextureFromFile(path));
                 }
 
+                if (example)
+                {
+                    logger.LogWarning($"Skipping a json suit because it's example data");
+                    break;
+                }
+
                 ModdedSuit moddedSuit = new ModdedSuit(suitKVPs, armKVPs, suit.vanillaModel, ModdedSuitsManager.suitModelTechTypes[suit.vanillaModel], suit.modifications, suit.stillsuitValues);
                 moddedSuit.jsonSuit = true;
                 ModdedSuitsManager.AddModdedSuit(moddedSuit);
             }
+        }
 
-            logger.LogInfo("Finished loading suit Jsons");
-
+        private void AddModdedGloves()
+        {
             List<JsonGloves> gloves = JsonLoader<JsonGloves>.LoadJsons(jsonFolder);
 
             foreach (JsonGloves item in gloves)
@@ -86,10 +109,24 @@ namespace SuitLib
                 }
 
                 Dictionary<string, Texture2D> armKVPs = new Dictionary<string, Texture2D>();
+                bool example = false;
+
                 foreach (var glovesKey in item.gloveTexturePairs.Keys)
                 {
+                    if (item.gloveTexturePairs[glovesKey].ToLower().Contains("example"))
+                    {
+                        example = true;
+                        break;
+                    }
+
                     string path = Path.Combine(jsonTexturesFolder, item.gloveTexturePairs[glovesKey]);
                     armKVPs.Add(glovesKey, ImageUtils.LoadTextureFromFile(path));
+                }
+
+                if (example)
+                {
+                    logger.LogWarning($"Skipping a json suit because it's example data");
+                    break;
                 }
 
                 ModdedGloves moddedGloves = new ModdedGloves(armKVPs, item.vanillaModel, ModdedSuitsManager.gloveModelTechTypes[item.vanillaModel], item.modifications, item.stillsuitValues);
