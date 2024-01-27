49d13056bf02ad293aeb71c52a20732e2418d989
Indigocoder1 <caleb@liston.us>
2024-01-27 09:13:38 -0800

    Added dev commands

---
+++ DevCommands/Commands.cs
@@ -0,0 +1,25 @@
+﻿using Nautilus.Commands;
+
+namespace DevCommands
+{
+    public static class Commands
+    {
+        [ConsoleCommand("devGamemode")]
+        public static string SetDevGamemodeCommand()
+        {
+            GameModeUtils.SetGameMode(GameModeOption.NoOxygen | GameModeOption.NoRadiation | GameModeOption.NoAggression | GameModeOption.NoCost
+                , GameModeOption.None);
+            return "";
+        }
+
+        [ConsoleCommand("devItems")]
+        public static string DevItemsCommand()
+        {
+            DevConsole.SendConsoleCommand("item seaglide");
+            DevConsole.SendConsoleCommand("item scanner");
+            DevConsole.SendConsoleCommand("item builder");
+
+            return "";
+        }
+    }
+}
---
+++ DevCommands/Main.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+
+namespace DevCommands
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string myGUID = "Indigocoder.DevCommands";
+        private const string pluginName = "Dev Commands";
+        private const string versionString = "1.0.0";
+
+        public static ManualLogSource logger;
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        private void Awake()
+        {
+            logger = Logger;
+
+            ConsoleCommandsHandler.RegisterConsoleCommands(typeof(Commands));
+            
+            Logger.LogInfo($"{pluginName} {versionString} Loaded.");
+        }
+    }
+}
