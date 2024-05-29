ababbdc4a58c3fbd2ae4c442a1803e2e82332eef
Indigocoder1 <caleb@liston.us>
2024-05-29 13:36:41 -0700

    Initial commit

---
+++ TextureReplacerEditor/Main_Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace TextureReplacerEditor
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInDependency("Indigocoder.TextureReplacer", BepInDependency.DependencyFlags.HardDependency)]
+    public class Main_Plugin : BaseUnityPlugin
+    {
+        private const string myGUID = "Indigocoder.TextureReplacerEditor";
+        private const string pluginName = "Texture Replacer Editor";
+        private const string versionString = "1.0.0";
+
+        public static ManualLogSource logger;
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        private void Awake()
+        {
+            logger = Logger;
+
+            harmony.PatchAll();
+
+            Logger.LogInfo($"{pluginName} {versionString} Loaded.");
+        }
+    }
+}
---
+++ TextureReplacerEditor/Patches/PlayerPatches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace TextureReplacerEditor.Patches
+{
+    [HarmonyPatch(typeof(Player))]
+    internal class PlayerPatches
+    {
+        [HarmonyPatch(nameof(Player.Update)), HarmonyPostfix]
+        private static void Update_Postfix(Player __instance)
+        {
+
+        }
+    }
+}
